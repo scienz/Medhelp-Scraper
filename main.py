d27a19f6d6baadccb1e6e179d7f9b3d6681994d4
Xinchen Zhang <xinchez4@uci.edu>
2020-03-04 12:32:56 -0800

    ?

--- main.py
+++
@@ -1,8 +0,0 @@
-import frontier
-import post_scraper
-
-if __name__ == "__main__":
-    lst = frontier.build()
-    dct = lst[0]
-    for key in dct:
-        post_scraper.run(key, dct[key])
--- page_retriever.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-def get_page(url: str):
-    headers = {
-        "Referer": "https://www.medhelp.org",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36"
-    }
-    r = requests.get(url, headers = headers)
-    return r.text
