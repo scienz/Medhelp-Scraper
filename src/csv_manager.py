64b2943ec6d54948f6cabdde5dfdd0acad4bd802
Xinchen Zhang <xinchez4@uci.edu>
2020-03-04 12:29:36 -0800

    py removed

--- src/csv_manager.py
+++
@@ -1,7 +0,0 @@
-import csv
-
-def save_as_csv(file_name, mode, data: [[]]):
-    with open(file_name, mode) as f:
-        fw = csv.writer(f, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-        for row in data:
-            fw.writerow(row)
--- src/str_format.py
+++
@@ -1,8 +0,0 @@
-'''
-General-purpose module used to format the output.
-'''
-
-import re
-
-def elim_space(text):
-    return re.sub(r'\s+', '', text)
